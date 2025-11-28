28a2ad7ab5a374d624030974fec8bc5cb989ce2b
Adam Robson <adamrayrobson@gmail.com>
2025-11-27 20:57:03 -0800

    fix import errors

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useTheme } from '@/app/contexts/theme-provider';
+import { useTheme } from '@/contexts/theme-provider';
 
 export default function Logo() {
   const { theme } = useTheme();
